eae63a14d76a377fde73b3419b5d3d953bfe4f18
VotreNom <kobeameth@gmail.com>
2025-08-13 03:35:01 +0200

    Config files

--- src/main/java/com/mgaye/banking_application/config/RedisConfig.java
+++ src/main/java/com/mgaye/banking_application/config/RedisConfig.java
@@ -1,60 +1,10 @@
-// package com.mgaye.banking_application.config;
-
-// import org.springframework.beans.factory.annotation.Value;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-// import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
-// import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-// import org.springframework.data.redis.core.RedisTemplate;
-// import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-// import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-// import io.lettuce.core.ClientOptions;
-
-// @Configuration
-// @EnableRedisRepositories
-// public class RedisConfig {
-
-//     @Value("${spring.redis.host}")
-//     private String redisHost;
-
-//     @Value("${spring.redis.port}")
-//     private int redisPort;
-
-//     // @Value("${spring.redis.password}")
-//     // private String redisPassword;
-
-//     @Bean
-//     public LettuceConnectionFactory redisConnectionFactory() {
-//         RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
-//         config.setHostName(redisHost);
-//         config.setPort(redisPort);
-//         // config.setPassword(redisPassword);
-
-//         LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
-//                 .clientOptions(ClientOptions.builder()
-//                         .autoReconnect(true)
-//                         .build())
-//                 .build();
-
-//         return new LettuceConnectionFactory(config, clientConfig);
-//     }
-
-//     @Bean
-
-//     public RedisTemplate<String, String> redisTemplate() {
-//         RedisTemplate<String, String> template = new RedisTemplate<>();
-//         template.setConnectionFactory(redisConnectionFactory());
-//         template.setDefaultSerializer(new StringRedisSerializer());
-//         return template;
-//     }
-// }
 package com.mgaye.banking_application.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -63,10 +13,6 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import io.lettuce.core.ClientOptions;
-
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
-import java.time.Duration;
 
 @Configuration
 @EnableRedisRepositories
@@ -81,55 +27,119 @@ public class RedisConfig {
     @Value("${spring.redis.password}")
     private String redisPassword;
 
-    @Value("${spring.redis.timeout:5000}")
-    private int timeout;
-
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
         RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
         config.setHostName(redisHost);
         config.setPort(redisPort);
-        config.setPassword(redisPassword);
-        config.setDatabase(0);
+        config.setPassword(RedisPassword.of(redisPassword));
 
         LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
-                .commandTimeout(Duration.ofMillis(timeout))
-                .shutdownTimeout(Duration.ofMillis(2000))
+                .clientOptions(ClientOptions.builder()
+                        .autoReconnect(true)
+                        .build())
                 .build();
 
         return new LettuceConnectionFactory(config, clientConfig);
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory());
-
-        // Key serializers
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setHashKeySerializer(new StringRedisSerializer());
-
-        // Value serializers
-        GenericJackson2JsonRedisSerializer jsonSerializer = new GenericJackson2JsonRedisSerializer();
-
-        template.setValueSerializer(jsonSerializer);
-        template.setHashValueSerializer(jsonSerializer);
-
-        // Transaction support
-        template.setEnableTransactionSupport(true);
-        template.afterPropertiesSet();
-
-        return template;
-    }
-
-    // Banking-specific template for counters
-    @Bean
-    public RedisTemplate<String, Long> counterTemplate() {
-        RedisTemplate<String, Long> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory());
+    public RedisTemplate<String, String> redisTemplate(LettuceConnectionFactory connectionFactory) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new StringRedisSerializer());
-        template.setEnableTransactionSupport(true);
         return template;
     }
 }
+
+// package com.mgaye.banking_application.config;
+
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import
+// org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+// import
+// org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+// import
+// org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+// import org.springframework.data.redis.core.RedisTemplate;
+// import
+// org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+// import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+// import io.lettuce.core.ClientOptions;
+
+// import
+// org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+// import org.springframework.data.redis.serializer.StringRedisSerializer;
+// import java.time.Duration;
+
+// @Configuration
+// @EnableRedisRepositories
+// public class RedisConfig {
+
+// @Value("${spring.redis.host}")
+// private String redisHost;
+
+// @Value("${spring.redis.port}")
+// private int redisPort;
+
+// @Value("${spring.redis.password}")
+// private String redisPassword;
+
+// @Value("${spring.redis.timeout:5000}")
+// private int timeout;
+
+// @Bean
+// public LettuceConnectionFactory redisConnectionFactory() {
+// RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+// config.setHostName(redisHost);
+// config.setPort(redisPort);
+// config.setPassword(redisPassword);
+// config.setDatabase(0);
+
+// LettuceClientConfiguration clientConfig =
+// LettuceClientConfiguration.builder()
+// .commandTimeout(Duration.ofMillis(timeout))
+// .shutdownTimeout(Duration.ofMillis(2000))
+// .build();
+
+// return new LettuceConnectionFactory(config, clientConfig);
+// }
+
+// @Bean
+// public RedisTemplate<String, Object> redisTemplate() {
+// RedisTemplate<String, Object> template = new RedisTemplate<>();
+// template.setConnectionFactory(redisConnectionFactory());
+
+// // Key serializers
+// template.setKeySerializer(new StringRedisSerializer());
+// template.setHashKeySerializer(new StringRedisSerializer());
+
+// // Value serializers
+// GenericJackson2JsonRedisSerializer jsonSerializer = new
+// GenericJackson2JsonRedisSerializer();
+
+// template.setValueSerializer(jsonSerializer);
+// template.setHashValueSerializer(jsonSerializer);
+
+// // Transaction support
+// template.setEnableTransactionSupport(true);
+// template.afterPropertiesSet();
+
+// return template;
+// }
+
+// // Banking-specific template for counters
+// @Bean
+// public RedisTemplate<String, Long> counterTemplate() {
+// RedisTemplate<String, Long> template = new RedisTemplate<>();
+// template.setConnectionFactory(redisConnectionFactory());
+// template.setKeySerializer(new StringRedisSerializer());
+// template.setValueSerializer(new StringRedisSerializer());
+// template.setEnableTransactionSupport(true);
+// return template;
+// }
+// }
